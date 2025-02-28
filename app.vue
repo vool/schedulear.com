ff248ee458a7eb23080e73a3959ed27281c9ac00
Keith <keith@vool.ie>
2025-02-28 20:35:19 +0000

    clean

--- app.vue
+++ app.vue
@@ -1,8 +1,11 @@
 <template>
-<div class="min-h-screen flex space-y-1 w-full fixed bg-lusta-50 ">
-  <div class="flex-1 p-2  relative bg-shaft-950 text-lusta-50">
 
 
+
+<div class="min-h-screen flex space-y-1 w-full fixed bg-lusta ">
+
+
+  <div class="flex-1 p-2 relative bg-shaft text-lusta">
     <div class="p-8  ">
       <ImgsLogo />
     </div>
@@ -17,7 +20,7 @@
   <div class="flex-1 relative">
 
     <div class="p-8  float-right ">
-      ...organise the world for the ear.
+      ...organise the world for the ear
     </div>
 
 
@@ -25,22 +28,22 @@
 
       <ul class="flex justify-center mt-5 space-x-5">
         <li>
-          <a href="https://mastodon.social/@schedulear" target="_blank" class="hover:text-white">
+          <a href="https://mastodon.social/@schedulear" target="_blank" class="text-shaft hover:text-black">
             <ImgsIconsMastodon />
           </a>
         </li>
         <li>
-          <a href="https://bsky.app/profile/schedulear.bsky.social" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+          <a href="https://bsky.app/profile/schedulear.bsky.social" target="_blank" class="text-shaft hover:text-black">
             <ImgsIconsBluesky />
           </a>
         </li>
         <li>
-          <a href="https://www.instagram.com/schedulear_" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+          <a href="https://www.instagram.com/schedulear_" target="_blank" class="text-shaft hover:text-black">
             <ImgsIconsInstagram />
           </a>
         </li>
         <li>
-          <a href="mailto:hello@schedulear.com" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+          <a href="mailto:hello@schedulear.com" target="_blank" class="text-shaft hover:text-black">
             <ImgsIconsEmail />
           </a>
         </li>
--- components/imgs/Name.vue
+++ components/imgs/Name.vue
@@ -4,69 +4,50 @@
    width="55.86705mm"
    height="8.5931644mm"
    viewBox="0 0 55.86705 8.5931644"
-   version="1.1"
-   id="svg925"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg">
-  <defs
-     id="defs922" />
+   xmlns="http://www.w3.org/2000/svg">
   <g
-     id="layer1"
      transform="translate(-34.829694,-158.23011)">
     <g
-       aria-label="schedulear"
        transform="matrix(0.27046996,0,0,0.27046996,-267.14473,-129.4465)"
-       id="text2646"
-       style="font-style:oblique;font-weight:bold;font-size:40px;line-height:0;font-family:Umpush;-inkscape-font-specification:'Umpush Bold Oblique';letter-spacing:0px;word-spacing:0px;white-space:pre;shape-inside:url(#rect2648);fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538">
+       style="fill:currentColor;">
       <g
-         id="g9083"
          transform="translate(0.06415501,67.186335)"
-         style="fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538">
+         style="fill:currentColor;">
         <path
            d="m 1125.9363,1025.3113 c 5.08,0 8.72,-2.28 8.72,-6.28 0,-3.48 -1.96,-5.24 -8.6,-6.32 -3.48,-0.56 -4.72,-1.32 -4.72,-3.2 0,-1.68 1.32,-2.88 4.04,-2.88 2.64,0 4.2,1.48 4.6,3.68 l 4.36,-0.24 c -0.6,-3.88 -3.44,-7 -8.92,-7 -5.52,0 -8.48,2.72 -8.48,6.6 0,3.48 1.96,5.32 8.28,6.44 4.24,0.72 5.04,1.4 5.04,2.96 0,1.88 -1.68,2.68 -4.32,2.68 -3.24,0 -4.76,-1.48 -5.16,-3.84 l -4.36,0.2 c 0.32,4.2 3.52,7.2 9.52,7.2 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9069" />
+           />
         <path
            d="m 1147.6963,1025.3113 c 5.2,0 8.84,-3.16 9.52,-8.04 l -4.44,-0.24 c -0.4,3.08 -2.36,4.6 -5.08,4.6 -3.6,0 -5.68,-2.76 -5.68,-7.44 0,-4.68 2.08,-7.44 5.68,-7.44 2.68,0 4.64,1.52 5.08,4.28 l 4.44,-0.24 c -0.72,-4.92 -4.36,-7.72 -9.52,-7.72 -6.12,0 -10.08,4.4 -10.08,11.12 0,6.72 3.96,11.12 10.08,11.12 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9071" />
+          />
         <path
            d="m 1161.0563,1024.8313 h 4.24 v -12.48 c 0,-3.72 2,-5.72 5.12,-5.72 2.72,0 4.04,1.84 4.04,5.64 v 12.56 h 4.24 v -13.68 c 0,-4.76 -2.52,-8.08 -7.16,-8.08 -2.8,0 -5.12,1.12 -6.24,3.56 v -10.20002 h -4.24 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9073" />
+            />
         <path
            d="m 1192.9762,1025.3113 c 4.68,0 8.16,-2.64 9.28,-6.6 l -4.4,-0.28 c -0.68,2 -2.36,3.24 -4.84,3.24 -3.4,0 -5.52,-2.08 -5.72,-6.24 h 15.28 v -1.08 c 0,-7.16 -3.92,-11.28 -9.72,-11.28 -6.08,0 -9.96,4.4 -9.96,11.12 0,6.72 3.88,11.12 10.08,11.12 z m -5.68,-13.08 c 0.48,-3.6 2.52,-5.56 5.56,-5.56 2.88,0 4.96,1.8 5.24,5.56 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9075" />
+           />
         <path
            d="m 1214.4161,1025.3113 c 2.96,0 5.48,-1.36 6.68,-3.68 l 0.12,3.2 h 3.96 v -28.40002 h -4.24 v 10.12002 c -1.16,-2.2 -3.6,-3.48 -6.52,-3.48 -5.44,0 -8.88,4.32 -8.88,11.12 0,6.84 3.4,11.12 8.88,11.12 z m 1.12,-3.68 c -3.4,0 -5.6,-2.88 -5.6,-7.44 0,-4.64 2.2,-7.44 5.6,-7.44 3.48,0 5.4,2.72 5.4,7.44 0,4.68 -1.92,7.44 -5.4,7.44 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9077" />
+            />
         <path
            d="m 1237.4161,1025.3113 c 3.08,0 5.32,-1.36 6.44,-3.96 l 0.04,3.48 h 3.96 v -21.28 h -4.24 v 12.48 c 0,3.64 -1.84,5.72 -4.92,5.72 -2.72,0 -4.04,-1.72 -4.04,-5.6 v -12.6 h -4.24 v 13.68 c 0,5.08 2.68,8.08 7,8.08 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9079" />
+            />
         <path
            d="m 1258.496,1024.8313 h 2.76 v -3.48 h -1.68 c -0.96,0 -1.48,-0.44 -1.48,-1.44 v -23.48002 h -4.24 v 23.88002 c 0,3.16 1.84,4.52 4.64,4.52 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9081" />
+           />
       </g>
       <g
          id="g9091"
          transform="rotate(4.3061023,376.23981,1059.0693)"
-         style="fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538">
+         style="fill:currentColor;">
         <path
            d="m 1273.6159,1025.3113 c 4.68,0 8.16,-2.64 9.28,-6.6 l -4.4,-0.28 c -0.68,2 -2.36,3.24 -4.84,3.24 -3.4,0 -5.52,-2.08 -5.72,-6.24 h 15.28 v -1.08 c 0,-7.16 -3.92,-11.28 -9.72,-11.28 -6.08,0 -9.96,4.4 -9.96,11.12 0,6.72 3.88,11.12 10.08,11.12 z m -5.68,-13.08 c 0.48,-3.6 2.52,-5.56 5.56,-5.56 2.88,0 4.96,1.8 5.24,5.56 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9085" />
+        />
         <path
            d="m 1293.8958,1025.3113 c 3.32,0 6.12,-1.52 7.2,-3.76 0.32,2.48 1.76,3.4 4.2,3.4 0.64,0 1.28,-0.04 1.56,-0.12 v -3.36 h -0.76 c -0.8,0 -1.12,-0.32 -1.12,-1.36 v -8.28 c 0,-5.52 -3.08,-8.76 -8.84,-8.76 -5.04,0 -8.36,2.64 -9.16,6.88 l 4.36,0.28 c 0.56,-2.32 2.08,-3.56 4.8,-3.56 3.04,0 4.6,1.64 4.6,4.92 l -7.32,1.4 c -4.68,0.92 -6.8,2.88 -6.8,6.52 0,3.68 2.84,5.8 7.28,5.8 z m 0.68,-3.36 c -2.28,0 -3.56,-1 -3.56,-2.84 0,-1.64 0.92,-2.64 3.48,-3.12 l 6.24,-1.2 v 1.36 c 0,3.4 -2.32,5.8 -6.16,5.8 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9087" />
+         />
         <path
            d="m 1310.4158,1024.8313 h 4.24 v -12.56 c 0,-3.36 1.6,-4.92 4.84,-4.92 h 2.04 v -3.8 h -2.04 c -2.6,0 -4.28,1.24 -5.08,4.04 l -0.12,-4.04 h -3.88 z"
-           style="font-style:normal;font-weight:500;font-family:Geist;-inkscape-font-specification:'Geist, Medium';fill:#0a0b0e;fill-opacity:1;stroke-width:7.34538"
-           id="path9089" />
+         />
       </g>
     </g>
   </g>
